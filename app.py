a7bc62d4b0585720a43aef5fc6c8548809dc0ed7
spaceboi21 <ma_abbas2001@hotmail.com>
2024-03-11 04:35:32 +0500

    debug errors

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 from flask import Flask, request, render_template
 from werkzeug.utils import secure_filename
 from doctr.io import DocumentFile
@@ -13,8 +12,8 @@ os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 app.config['RESULTS_FOLDER'] = 'results/'
 os.makedirs(app.config['RESULTS_FOLDER'], exist_ok=True)
 
-olevel_keywords = ["general certificate of education", "gce ordinary level", "o level"] 
-alevel_keywords = ["general certificate of education", "gce advanced level", "a level"]
+olevel_keywords = ["ordinary level", "o level"] 
+alevel_keywords = ["Advanced Subsidiary", "Advanced Level"]
 cnic_keywords = ["national identity card", "nadra", "cnic"]
 ielts_keywords = ["international english language testing system", "ielts", "british council"]
 sat_keywords = ["sat", "scholastic aptitude test", "college board"]
@@ -35,21 +34,20 @@ def classify_document(text):
     else:
         return "Unknown Document Type"
 
-
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
-    if 'files' not in request.files:
+    if 'certificates[]' not in request.files:
         return 'No file part'
 
     name = request.form.get('name', '').lower()
     cnic = request.form.get('cnic', '').lower()
     passport_number = request.form.get('passport_number', '').lower()
 
-    files = request.files.getlist('certificates[]')  # Key change here!
+    files = request.files.getlist('certificates[]')
     results = []
 
     for file in files:
@@ -85,9 +83,6 @@ def upload_file():
         document_type = classify_document(full_text)  
         results.append((filename, status, result_info, document_type))
 
-
-    response = "<br>".join([f"{filename}: {status}<br>{', <br>'.join(result_info)}\n" for filename, status, result_info in results])
-    # return response if results else 'No files processed'
     return render_template('results_page.html', results=results)
 
 if __name__ == '__main__':
